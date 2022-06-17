c91d7555e74040cc0be227b6d8fa1a14827ce1e0
IVlaD17 <vlad.ionica@stfc.ac.uk>
2022-06-17 13:34:02 +0100

    Identified the main tests for the CFAGroup class

---
+++ test/test_cfa_group.cpp
@@ -0,0 +1,13 @@
+// Test the getParentGroup, getGroupCount and all getGroup methods here
+bool test_get_group() { return true; }
+// Test all getVar methods here
+bool test_get_var() { return true; }
+// Test all getDim methods here
+bool test_get_dim() { return true; }
+
+// Test all addGroup methods here
+bool test_add_group() { return true; }
+// Test all addVar methods here
+bool test_add_var() { return true; }
+// Test all addDim methods here
+bool test_add_dim() { return true; }
