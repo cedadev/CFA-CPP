1e1baeb2c3858abf07a81d8f6d92ca40fe43bf6b
IVlaD17 <vlad.ionica@stfc.ac.uk>
2022-06-22 16:01:49 +0100

    Preliminary implementation of some CFAGroup methods

--- src/CFAGroup.cpp
+++ src/CFAGroup.cpp
@@ -5,4 +5,27 @@ CFAGroup::CFAGroup(std::string name) { this->name = name; }
 std::string CFAGroup::getName() { return name; }
 int CFAGroup::getId() { return id; }
 
-CFAGroup CFAGroup::getParentGroup() { return *parentGroup; }
+CFAGroup CFAGroup::getParentGroup() { return *parent; }
+CFAGroup CFAGroup::getGroup(int groupId) 
+{  
+    for(CFAGroup group : groups)
+        if(group.getId() == groupId)
+            return group;
+}
+CFAGroup CFAGroup::getGroup(std::string name) 
+{  
+    for(CFAGroup group : groups)
+        if(group.getName() == name)
+            return group;
+}
+CFAGroup CFAGroup::addGroup(std::string name) 
+{ 
+    groups.push_back(name);
+    groups.back().id = 0;
+    groups.back().parent = this;
+    return groups.back();
+}
+CFAGroup CFAGroup::addGroup(std::string name)
+{
+    return groups.emplace_back(name, 0, this);
+}
--- src/CFAGroup.h
+++ src/CFAGroup.h
@@ -17,9 +17,11 @@ class CFAGroup
         int id;
         std::string name;
 
-        CFAGroup* parentGroup;
+        CFAGroup* parent;
+        std::vector<CFAGroup> groups;
     public:
         CFAGroup(std::string name);
+        CFAGroup(std::string name, int id, CFAGroup* parent);
         
         int getId();
         std::string getName();
