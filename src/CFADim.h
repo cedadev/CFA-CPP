22a0c811c56652ee59ae6bc67c1cde725384eb3d
IVlaD17 <vlad.ionica@stfc.ac.uk>
2022-06-29 13:32:49 +0100

    Implemented CFADim

--- src/CFADim.h
+++ src/CFADim.h
@@ -1,19 +1,25 @@
 #pragma once
 
 #include <string>
+#include "CFAGroup.h"
 
 // declare CFA-C library as external C library
 extern "C" {
     #include "cfa.h"
 }
 
-class CFAGroup;
-
 class CFADim
 {
+    private:
+        int id;
+        int dimLen;
+        cfa_type type;
+        std::string name;
+
+        CFAGroup parent;
     public:
-        CFADim(std::string name, cfa_type d_type);
-        CFADim(std::string name, cfa_type d_type, int dim_len);
+        CFADim(std::string name, cfa_type type);
+        CFADim(std::string name, cfa_type type, int dimLen, CFAGroup parent);
         
         int getId();
         int getLen();
