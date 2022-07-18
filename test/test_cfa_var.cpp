c420102c93422c32f345a4ea6bdf5ea43daccd75
IVlaD17 <vlad.ionica@stfc.ac.uk>
2022-07-18 15:39:10 +0100

    Wrote most of the CFAVar tests

--- test/test_cfa_var.cpp
+++ test/test_cfa_var.cpp
@@ -21,7 +21,17 @@ void test_cfa_var_creation()
 
 void test_cfa_var_get_dim_count() 
 {
+    CFA::File file(filePath, CFA::CFANetCDF, CFA::Write);
+    CFA::Group group(file.getId(), groupName);
+    CFA::Var var(group.getId(), varName, 0);
 
+    CFA::Dim dim1(group.getId(), "CFA Dim 1", 0);
+    CFA::Dim dim2(group.getId(), "CFA Dim 2", 0);
+    CFA::Dim dim3(group.getId(), "CFA Dim 3", 0);
+
+    std::vector<int> dimIds = {dim1.getId(), dim2.getId(), dim3.getId()};
+    var.updateDims(dimIds);
+    assert(var.getDimCount() == 3);
 }
 
 void test_cfa_var_get_name()
@@ -44,22 +54,74 @@ void test_cfa_var_get_type()
 
 void test_cfa_var_get_dim()
 {
+    CFA::File file(filePath, CFA::CFANetCDF, CFA::Write);
+    CFA::Group group(file.getId(), groupName);
+    CFA::Var var(group.getId(), varName, 0);
 
+    CFA::Dim dim1(group.getId(), "CFA Dim 1", 0);
+    CFA::Dim dim2(group.getId(), "CFA Dim 2", 0);
+    CFA::Dim dim3(group.getId(), "CFA Dim 3", 0);
+
+    std::vector<int> dimIds = {dim1.getId(), dim2.getId(), dim3.getId()};
+    var.updateDims(dimIds);
+    assert(var.getDim(0).getId() == dim1.getId());
+    assert(var.getDim(1).getId() == dim2.getId());
+    assert(var.getDim(2).getId() == dim3.getId());
 }
 
 void test_cfa_var_get_dims()
 {
+    CFA::File file(filePath, CFA::CFANetCDF, CFA::Write);
+    CFA::Group group(file.getId(), groupName);
+    CFA::Var var(group.getId(), varName, 0);
 
+    CFA::Dim dim1(group.getId(), "CFA Dim 1", 0);
+    CFA::Dim dim2(group.getId(), "CFA Dim 2", 0);
+    CFA::Dim dim3(group.getId(), "CFA Dim 3", 0);
+
+    std::vector<int> dimIds = {dim1.getId(), dim2.getId(), dim3.getId()};
+    var.updateDims(dimIds);
+
+    std::vector<CFA::Dim> dims = var.getDims();
+    for(int i = 0; i < dims.size(); i++)
+    {
+        assert(dims[i].getId() == dimIds[i]);
+    }
 }
 
 void test_cfa_var_get_dim_names()
 {
+    CFA::File file(filePath, CFA::CFANetCDF, CFA::Write);
+    CFA::Group group(file.getId(), groupName);
+    CFA::Var var(group.getId(), varName, 0);
 
+    CFA::Dim dim1(group.getId(), "CFA Dim 1", 0);
+    CFA::Dim dim2(group.getId(), "CFA Dim 2", 0);
+    CFA::Dim dim3(group.getId(), "CFA Dim 3", 0);
+
+    std::vector<int> dimIds = {dim1.getId(), dim2.getId(), dim3.getId()};
+    var.updateDims(dimIds);
+
+    std::vector<std::string> dimNames = var.getDimNames();
+    assert(dimNames[0] == dim1.getName());
+    assert(dimNames[1] == dim2.getName());
+    assert(dimNames[2] == dim3.getName());
 }
 
 void test_cfa_var_update_dim()
 {
+    CFA::File file(filePath, CFA::CFANetCDF, CFA::Write);
+    CFA::Group group(file.getId(), groupName);
+    CFA::Var var(group.getId(), varName, 0);
 
+    CFA::Dim dim1(group.getId(), "CFA Dim 1", 0);
+    CFA::Dim dim2(group.getId(), "CFA Dim 2", 0);
+    CFA::Dim dim3(group.getId(), "CFA Dim 3", 0);
+
+    std::vector<int> dimIds = {dim1.getId(), dim2.getId(), dim3.getId()};
+    var.updateDims(dimIds);
+
+    assert(var.getDimCount() == 3);
 }
 
 int main(void)
